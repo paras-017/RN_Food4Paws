a14694f4593f9489d3f7436133d6a60937da252f
paras-017 <paraschauhan0017@gmail.com>
2023-08-19 15:12:37 +0530

    Home Screen UI done

--- Food4Paws/Screens/HomeScreen.jsx
+++ Food4Paws/Screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Category from '../components/Category'
@@ -6,12 +6,42 @@ import Carousel from '../components/Carousel'
 
 const HomeScreen = () => {
   return (
-    <View>
+    <ScrollView>
+      <View>
       <Navbar/>
       <Category/>
       <Carousel/>
+      <View className='space-y-3 mb-3'>
+        <Image className='h-32 w-screen' source={require('../assets/offers/1.jpg')}/>
+        <Image className='h-32 w-screen' source={require('../assets/offers/2.jpg')}/>
+        <Image className='h-32 w-screen' source={require('../assets/offers/3.jpg')}/>
+      </View>
+
+      {/* ✨Explore By Pet Category ✨ */}
+      <View>
+          <Text className='text-black text-center text-xl font-bold'>✨Explore By Pet Category ✨</Text>
+          <View className='flex items-center space-y-3 my-3'>
+            <View className='flex-row space-x-4'>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/cat.jpg')}/>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/dog.jpg')}/>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/birds.jpg')}/>
+            </View>
+            <View className='flex-row space-x-4'>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/fish.jpg')}/>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/guineaPig.jpg')}/>
+              <Image className='h-16 w-28 rounded-2xl' source={require('../assets/HomePage/petCategories/rabbit.jpg')}/>
+            </View>
+          </View>
+      </View>
       
+      {/* Our Sponsors */}
+      <View>
+        <View>
+          
+        </View>
+      </View>
     </View>
+    </ScrollView>
   )
 }
 
