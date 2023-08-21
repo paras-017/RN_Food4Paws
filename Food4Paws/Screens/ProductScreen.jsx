12546cb5812bcd69c9448195c22f3d23ff18d42d
paras-017 <paraschauhan0017@gmail.com>
2023-08-21 15:00:23 +0530

    search functionality adde

--- Food4Paws/Screens/ProductScreen.jsx
+++ Food4Paws/Screens/ProductScreen.jsx
@@ -1,19 +1,27 @@
-import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
-import React from 'react'
-import * as Icon from "react-native-feather";
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar';
-import { products } from '../components/product';
 import ProductCard from '../components/ProductCard';
-// import { useProductContext } from '../context/productContext';
+import { useFilterContext } from '../context/FilterContext';
 
-const ProductScreen = ({route}) => {
- const navigation = useNavigation()
- console.log(products.length)
+const ProductScreen = () => {
+ const {filter_products } = useFilterContext()
+ const [searchText, setSearchText] = useState('');
+ const filteredProducts = filter_products.filter(product =>
+  product.name.toLowerCase().includes(searchText.toLowerCase())
+);
   return (
     <View className='mb-16'>
       <Navbar wantLogo={false}/>
-      <FlatList numColumns={2}  data={products}  keyExtractor={item => item._id} renderItem={({item})=>{
+      <View>
+        <TextInput
+        value={searchText}
+        onChangeText={setSearchText} 
+        placeholder="Search by name"
+        placeholderTextColor='gray'
+        className='border-2 text-black h-9 w-48 absolute bottom-4 rounded-lg left-14'/>
+      </View>
+      <FlatList numColumns={2}  data={filteredProducts}  keyExtractor={item => item._id} renderItem={({item})=>{
       return (
         <View className='flex-1 justify-center items-center' >
           <ProductCard {...item}/>
--- Food4Paws/components/Navbar.jsx
+++ Food4Paws/components/Navbar.jsx
@@ -2,9 +2,11 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import * as Icon from "react-native-feather";
 import { useNavigation } from '@react-navigation/native';
+import { useCartContext } from '../context/cartContext';
 
 const Navbar = ({wantLogo}) => {
   const navigation = useNavigation()
+  const {total_item}= useCartContext()
   return (
     <View className='flex-row justify-between  p-3'>
       <View  className='h-11  w-44  justify-center'>
@@ -16,11 +18,13 @@ const Navbar = ({wantLogo}) => {
          </TouchableOpacity>}
       </View>
       <View className='flex-row items-center space-x-4 mr-2 '>
-        <Icon.Search height={25} width={25} stroke={'black'}/>
+        <TouchableOpacity onPress={()=>navigation.navigate('ProductScreen')}>
+         <Icon.Search height={25} width={25} stroke={'black'}/>
+        </TouchableOpacity>
         <Icon.Heart height={25} width={25} stroke={'black'}/>
         <View className='relative'>
          <Icon.ShoppingCart height={25} width={25} stroke={'black'}/>
-         <Text className='text-black absolute bg-blue-400 h-5 w-5 text-center rounded-full bottom-4 left-3'>5</Text>
+         <Text className='text-black absolute bg-blue-400 h-5 w-5 text-center rounded-full bottom-4 left-3'>{total_item}</Text>
         </View>
       </View>
     </View>
